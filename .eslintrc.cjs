1418652b5151fb59699b97d714de4421b5f2c164
jpassgo <jeffpascoe9@gmail.com>
2023-09-23 12:53:11 -0500

    updating linting and settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier', // This line replaces 'prettier/@typescript-eslint'
   ],
   rules: {
     // Add your custom rules here
